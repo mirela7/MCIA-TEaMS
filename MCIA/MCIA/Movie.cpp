eb92d7e57e1ed92077b117970873122445eb566c
eddie77777 <eddiedragomir17@gmail.com>
2022-11-11 20:48:43 +0200

    Implemented assignment operators and move constructor.

--- MCIA/MCIA/Movie.cpp
+++ MCIA/MCIA/Movie.cpp
@@ -1,6 +1,8 @@
 #include "Movie.h"
 
-Movie::Movie(const uint16_t id, const std::string& title, const std::string& genre, const uint16_t releaseYear, const uint16_t duration)
+
+
+Movie::Movie(const int id, const std::string& title, const std::string& genre, const uint16_t releaseYear, const uint16_t duration)
 	: m_id(id)
 	, m_title(title)
 	, m_genre(genre)
@@ -11,4 +13,30 @@ Movie::Movie(const uint16_t id, const std::string& title, const std::string& gen
 Movie::Movie(const Movie & movie)
 {
 	*this = movie;
+}
+
+Movie::Movie(Movie&& movie) noexcept
+{
+	*this = std::move(movie);
+}
+
+Movie& Movie::operator=(const Movie& movie)
+{
+	m_id = movie.m_id;
+	m_title = movie.m_title;
+	m_genre = movie.m_genre;
+	m_releaseYear = movie.m_releaseYear;
+	m_duration = movie.m_duration;
+	return *this;
+}
+
+Movie& Movie::operator=(Movie&& movie) noexcept
+{
+	m_id = movie.m_id;
+	m_title = movie.m_title;
+	m_genre = movie.m_genre;
+	m_releaseYear = movie.m_releaseYear;
+	m_duration = movie.m_duration;
+	new(&movie) Movie;
+	return *this;
 }
--- MCIA/MCIA/Movie.h
+++ MCIA/MCIA/Movie.h
@@ -6,13 +6,16 @@ class Movie
 {
 public:
 	Movie() = default;
-	Movie(const uint16_t id, const std::string& title, const std::string& genre, const uint16_t releaseYear, const uint16_t duration);
+	Movie(const int id, const std::string& title, const std::string& genre, const uint16_t releaseYear, const uint16_t duration);
 	Movie(const Movie& movie);
+	Movie(Movie&& movie) noexcept;
 
 	Movie& operator=(const Movie& movie);
+	Movie& operator=(Movie&& movie) noexcept;
+
 
 private:
-	uint16_t m_id;
+	int m_id;
 	std::string m_title;
 	std::string m_genre;
 	uint16_t m_releaseYear;
