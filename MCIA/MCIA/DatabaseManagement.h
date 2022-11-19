20e458d3d7bb238a81dd03cc405817542d858093
SoranaAurelia <sorana.aurelia@gmail.com>
2022-11-19 16:31:48 +0200

    added storage as memeber in DB Manager

--- MCIA/MCIA/DatabaseManagement.h
+++ MCIA/MCIA/DatabaseManagement.h
@@ -1,9 +1,24 @@
 #pragma once
+#pragma warning(disable : 4996)
 #include "User.h"
 #include <sqlite_orm/sqlite_orm.h>
+#include <any>
 
 using namespace sqlite_orm;
 
+namespace {
+    auto getStorage() {
+        static auto storage = make_storage("DBtest.db",
+            make_table("user",
+                make_column("Id", &User::m_id, primary_key()),
+                make_column("name", &User::m_name),
+                make_column("password", &User::m_password)));
+
+        return storage;
+    }
+}
+
+using storage_type = decltype(getStorage());
 
 class DatabaseManagement
 {
@@ -25,21 +40,13 @@ protected:
     friend class User;
 
     DatabaseManagement() = default;
-    static auto& getStorage() {
-        static auto storage = make_storage("DBtest.db",
-           make_table("user",
-                make_column("Id", &User::m_id, primary_key()),
-                make_column("name", &User::m_name),
-                make_column("password", &User::m_password)));
-
-        return storage;
-    }
+    
 
 protected:
-     
-
+    static inline storage_type storage = getStorage();
     static DatabaseManagement* m_database;
 };
+
 
 template<typename T>
 int32_t DatabaseManagement::InsertElement(const T& el)
@@ -52,7 +59,7 @@ int32_t DatabaseManagement::InsertElement(const T& el)
 template<typename T>
 inline T DatabaseManagement::GetElementById(const int32_t id)
 {
-    auto& st = getStorage();
+    // auto& st = getStorage();
     // TODO FAIL PROOF GET
-    return st.get<T>(id);
+    return storage.get<T>(id);
 }
