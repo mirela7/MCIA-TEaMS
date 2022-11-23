3330180cc910302038e4f3a980078635ca4c73fb
eddie77777 <eddiedragomir17@gmail.com>
2022-11-23 17:57:22 +0200

    Added intermediary class to prepare parse function

---
+++ MCIA/MCIA/MovieIntermediary.cpp
@@ -0,0 +1,7 @@
+#include "MovieIntermediary.h"
+
+MovieIntermediary::MovieIntermediary(uint16_t id, const std::string& title, const std::string& genre)
+	:m_id(id)
+	,m_title(title)
+	,m_genre(genre)
+{}
---
+++ MCIA/MCIA/MovieIntermediary.h
@@ -0,0 +1,24 @@
+#pragma once
+#pragma warning(disable : 4996)
+#include <iostream>
+#include <sqlite_orm/sqlite_orm.h>
+
+class MovieIntermediary
+{
+public:
+	MovieIntermediary() = default;
+	MovieIntermediary(uint16_t id, const std::string& title, const std::string& genres);
+
+	void SetId(uint16_t id);
+	void SetTitle(const std::string& title);
+	void SetGenre(const std::string& genres);
+
+	uint16_t GetId() const;
+	std::string GetTitle() const;
+	std::string GetGenre() const;
+
+private:
+	uint16_t m_id;
+	std::string m_title;
+	std::string m_genre;
+};
