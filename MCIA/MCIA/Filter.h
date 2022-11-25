e0c5bcdd97180880c10b1e1d23ee47610c82102c
mirela7 <mirela.magdalena.mm@gmail.com>
2022-11-25 19:16:31 +0200

    Wireframe for classes that will be used for selection

---
+++ MCIA/MCIA/Filter.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <vector>
+
+
+
+class Filter
+{
+public:
+
+private:
+	std::string m_column;
+	std::vector<std::string> m_acceptedValues;
+};
+
---
+++ MCIA/MCIA/PagedSelection.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Filter.h"
+
+template<class TEntity>
+class PagedSelection
+{
+public:
+	static void select(std::vector<Filter> filters);
+
+
+private:
+};
+
