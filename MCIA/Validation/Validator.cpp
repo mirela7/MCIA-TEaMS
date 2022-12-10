c4332ca97362619cdac16a6cae3184e191da808c
mirela7 <mirela.magdalena.mm@gmail.com>
2022-12-10 20:35:28 +0200

    Removed static, added function implementation

--- MCIA/Validation/Validator.cpp
+++ MCIA/Validation/Validator.cpp
@@ -1,1 +1,21 @@
 #include "Validator.h"
+
+bool Validator::IsAlphaNumericOrSpecial(const std::string& string)
+{
+    return find_if(string.begin(), string.end(), IsNotAlphaNumericOrSpecial) == string.end();
+}
+
+bool Validator::IsBlank(const std::string& string)
+{
+    return string.size() == 0 || find_if(string.begin(), string.end(), IsNotBlank) == string.end();
+}
+
+bool Validator::IsNotBlank(const char c)
+{
+    return !isblank(c);
+}
+
+bool Validator::IsNotAlphaNumericOrSpecial(const char c)
+{
+    return !(isalnum(c) || c == '_' || c == '.');
+}
--- MCIA/Validation/Validator.h
+++ MCIA/Validation/Validator.h
@@ -9,9 +9,9 @@
 class Validator
 {
 public:
-	static bool IsAlphaNumericOrSpecial(const std::string& string);
-	static bool IsBlank(const std::string& string);
-	static bool IsNotBlank(const char c);
-	static bool IsNotAlphaNumericOrSpecial(const char c);
+	bool IsAlphaNumericOrSpecial(const std::string& string);
+	bool IsBlank(const std::string& string);
+	bool IsNotBlank(const char c);
+	bool IsNotAlphaNumericOrSpecial(const char c);
 };
 
