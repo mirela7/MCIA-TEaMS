74b944bf8042534946eafb0db1806dc0d176eb8b
mirela7 <mirela.magdalena.mm@gmail.com>
2022-12-18 13:48:12 +0200

    Modified QSS stylesheet and added temp solution for background bug

--- MCIA/QT/mainwindow.cpp
+++ MCIA/QT/mainwindow.cpp
@@ -7,16 +7,21 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QFile stylesFile("stylesheet.qss");
+    
+    QFile stylesFile("../../../stylesheet.qss");
     if(!stylesFile.exists())
         std::cerr << "File doesnt exist";
-
     stylesFile.open(QFile::ReadOnly);
-
     if (!stylesFile.isOpen())
         std::cerr << "File is not open";
     QString StyleSheet = QLatin1String(stylesFile.readAll());
     qApp->setStyleSheet(StyleSheet);
+
+    /* Temp solution */
+    const char* transparentBG = "QLabel { background-color : transparent; }";
+    ui->lblTitle->setStyleSheet(transparentBG);
+    ui->lblUsername->setStyleSheet(transparentBG);
+    ui->lblPassword->setStyleSheet(transparentBG);
 }
 
 MainWindow::~MainWindow()
