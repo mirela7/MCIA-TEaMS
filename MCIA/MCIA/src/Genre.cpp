d502e6d5963a2ab5906e1c154d9025ff3ae907c6
eddie77777 <eddiedragomir17@gmail.com>
2023-01-08 13:18:57 +0200

    Added genres in genre table

--- MCIA/MCIA/src/Genre.cpp
+++ MCIA/MCIA/src/Genre.cpp
@@ -35,20 +35,39 @@ void Genre::ParseGenres()
 	auto& storage = DatabaseManagement::GetInstance().GetStorage();
 	auto moviesGenres = storage.select(columns(&MovieIntermediary::GetGenre));
 	int id = 1;
+	Genre g0(0, "Unknown");
+	DatabaseManagement::GetInstance().GetStorage().replace(g0);
+	int count = 0;
 	for (auto& tpl : moviesGenres)
 	{
+		bool OK = 0;
 		std::string name = get<0>(tpl);
 		std::string genre = "";
 		for (int i = 0; i < name.size(); i++)
 		{
 			if (name[i] == '|')
 			{
-				Genre g(-1, genre);
-				DatabaseManagement::GetInstance().GetStorage().replace(g);
+				Genre g(id, genre);
+				auto genres = storage.select(columns(&Genre::GetName));
+				for (auto& genreName : genres)
+				{
+					if (get<0>(genreName) == genre)
+					{
+						OK = 1;
+					}
+				}
+				if (OK == 0)
+				{
+					DatabaseManagement::GetInstance().InsertElement(g);
+					id++;
+				}
 				genre = "";
 				i++;
 			}
 			genre.push_back(name[i]);
 		}
+		count++;
+		if (count % 1000 == 0)
+			std::cout << "Verified rows: " << count << '\n';
 	}
 }
