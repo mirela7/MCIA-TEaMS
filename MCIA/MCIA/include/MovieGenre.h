255c0f9e273aaabe2a06a73726172026a95b860d
eddie77777 <eddiedragomir17@gmail.com>
2023-01-08 16:15:01 +0200

    Link all movies with their genres in a separate table and update database for full functionality

--- MCIA/MCIA/include/MovieGenre.h
+++ MCIA/MCIA/include/MovieGenre.h
@@ -5,17 +5,17 @@ class MovieGenre
 {
 public:
 	MovieGenre() = default;
-	MovieGenre(const uint16_t movieId, const uint16_t genreId);
+	MovieGenre(const uint32_t movieId, const uint16_t genreId);
 
-	uint16_t GetMovieId() const;
+	uint32_t GetMovieId() const;
 	uint16_t GetGenreId() const;
 
-	void SetMovieId(const uint16_t movieId);
+	void SetMovieId(const uint32_t movieId);
 	void SetGenreId(const uint16_t genreId);
 
 	void LinkGenreMovie();
 private:
-	uint16_t m_movieId;
+	uint32_t m_movieId;
 	uint16_t m_genreId;
 };
 
--- MCIA/MCIA/src/MovieGenre.cpp
+++ MCIA/MCIA/src/MovieGenre.cpp
@@ -1,12 +1,12 @@
 #include "../include/MovieGenre.h"
 #include "../include/DatabaseManagement.h"
 
-MovieGenre::MovieGenre(const uint16_t movieId, const uint16_t genreId)
+MovieGenre::MovieGenre(const uint32_t movieId, const uint16_t genreId)
 	:m_movieId(movieId)
 	, m_genreId(genreId)
 {}
 
-uint16_t MovieGenre::GetMovieId() const
+uint32_t MovieGenre::GetMovieId() const
 {
 	return m_movieId;
 }
@@ -16,7 +16,7 @@ uint16_t MovieGenre::GetGenreId() const
 	return m_genreId;
 }
 
-void MovieGenre::SetMovieId(const uint16_t movieId)
+void MovieGenre::SetMovieId(const uint32_t movieId)
 {
 	m_movieId = movieId;
 }
@@ -35,7 +35,7 @@ void MovieGenre::LinkGenreMovie()
 	int count = 0;
 	for (auto& tpl : moviesGenres)
 	{
-		uint16_t movieId = get<0>(tpl);
+		uint32_t movieId = get<0>(tpl);
 		std::string name = get<1>(tpl);
 		std::string genre = "";
 		for (int i = 0; i < name.size(); i++)
@@ -48,7 +48,13 @@ void MovieGenre::LinkGenreMovie()
 					{
 						uint16_t genreId = get<0>(genreData);
 						MovieGenre element(movieId, genreId);
-						DatabaseManagement::GetInstance().GetStorage().replace(element);
+						try {
+							DatabaseManagement::GetInstance().GetStorage().replace(element);
+						}
+						catch (std::exception e)
+						{
+							std::cout << e.what();
+						}
 						break;
 					}
 				}
@@ -56,6 +62,19 @@ void MovieGenre::LinkGenreMovie()
 				i++;
 			}
 			genre.push_back(name[i]);
+			if (i == name.size() - 1)
+			{
+				for (auto& genreData : genres)
+				{
+					if (get<1>(genreData) == genre)
+					{
+						uint16_t genreId = get<0>(genreData);
+						MovieGenre element(movieId, genreId);
+						DatabaseManagement::GetInstance().GetStorage().replace(element);
+						break;
+					}
+				}
+			}
 		}
 		count++;
 		std::cout << count << '\n';
