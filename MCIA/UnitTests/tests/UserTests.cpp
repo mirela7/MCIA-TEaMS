d99b953e066becadbc0ad54f6ddd9e1bce2f091a
mirela7 <mirela.magdalena.mm@gmail.com>
2023-01-08 19:18:53 +0200

    tested user class

--- MCIA/UnitTests/tests/UserTests.cpp
+++ MCIA/UnitTests/tests/UserTests.cpp
@@ -53,3 +53,29 @@ TEST(UserTests, MoveConstructor) {
 	EXPECT_EQ(user.GetName(), "");
 	EXPECT_EQ(user.GetPassword(), "");
 }
+
+TEST(UserTests, SetterName) {
+	User user;
+	user.SetName("username");
+	EXPECT_STREQ(user.GetName().c_str(), "username");
+}
+
+TEST(UserTests, SetterPassword) {
+	User user;
+	user.SetPassword("password");
+	EXPECT_STREQ(user.GetPassword().c_str(), "password");
+}
+
+TEST(UserTests, SetterId) {
+	User user;
+	user.SetId(1);
+	EXPECT_EQ(user.GetId(), 1);
+}
+
+TEST(UserTests, WriteOperator) {
+	User user(1, "nume", "parola");
+	std::ostringstream sstr;
+	sstr << user;
+	EXPECT_STREQ(sstr.str().c_str(), "1 nume");
+
+}
