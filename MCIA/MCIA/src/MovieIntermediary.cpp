dfea13702d853957c7a1036864ec6c0029b810e6
mirela7 <mirela.magdalena.mm@gmail.com>
2023-01-11 21:28:14 +0200

    Movie intermediary tests

--- MCIA/MCIA/src/MovieIntermediary.cpp
+++ MCIA/MCIA/src/MovieIntermediary.cpp
@@ -1,9 +1,17 @@
 #include "../include/MovieIntermediary.h"
 
+MovieIntermediary::MovieIntermediary()
+	: m_id(0)
+	, m_title("")
+	, m_genre("")
+{
+
+}
+
 MovieIntermediary::MovieIntermediary(uint32_t id, const std::string& title, const std::string& genre)
-	:m_id(id)
-	,m_title(title)
-	,m_genre(genre)
+	: m_id(id)
+	, m_title(title)
+	, m_genre(genre)
 {}
 
 void MovieIntermediary::SetId(uint32_t id)
