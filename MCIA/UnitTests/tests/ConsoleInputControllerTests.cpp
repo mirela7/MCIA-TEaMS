1e79ba5b50d89bb289dc5664abc18f811ea91759
mirela7 <mirela.magdalena.mm@gmail.com>
2023-01-11 11:38:19 +0200

    Test run using stringstreams on console input controller

--- MCIA/UnitTests/tests/ConsoleInputControllerTests.cpp
+++ MCIA/UnitTests/tests/ConsoleInputControllerTests.cpp
@@ -1,6 +1,22 @@
 #include <gtest/gtest.h>
 #include <ConsoleInputController.h>
+#include <sstream>
 
-TEST(ConsoleInputControllerTest, Yedy) {
+TEST(ConsoleInputControllerTest, GatherMovieIdSpecializedSizeInpage) {
+	std::vector<Movie> moviesInPage;
+	moviesInPage.emplace_back(1, "TestTitle", 2000, 5.0);
+	ConsoleInputController cic;
+
+	/* expects invalidId due to size, read the next id that exists in page & in db */
+	std::string userInput("12345678910 1 2");
+	std::string expectedOutput;
+	expectedOutput.append(cic.OUT_PICK_ID);
+	expectedOutput.append(cic.OUT_ASK_VALID_ID);
+
+	std::istringstream inputStream(userInput);
+	std::ostringstream outputStream;
 	
+	int validId = cic.gatherMovieIdFromUser(moviesInPage, outputStream, inputStream);
+	EXPECT_EQ(validId, 1);
+	EXPECT_STREQ(outputStream.str().c_str(), expectedOutput.c_str());
 }
