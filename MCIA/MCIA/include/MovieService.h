96af6986eefddc2b3278de553ab85930868e28d1
mirela7 <mirela.magdalena.mm@gmail.com>
2023-01-13 14:34:39 +0200

    Implemented function to retreive movies by ids to be used for output from recommendation system

--- MCIA/MCIA/include/MovieService.h
+++ MCIA/MCIA/include/MovieService.h
@@ -9,10 +9,12 @@ class MovieService {
 public:	
 	DBPage<WatchedMovieDisplayer> GetWatchedMoviesOfUser(const uint16_t userId, const int page, const int nmbRowsPerPage);
 	DBPage<WishlistedMovieDisplayer> GetWishListOfUser(const uint16_t userId, const int page, const int nmbRowsPerPage);
+	DBPage<Movie> GetMovieListFromListOfIndices(const std::vector<uint32_t>& movieIds, const int page);
 	MovieInformationDisplayer GetMovieInformations(const uint32_t id);
 	void AddMovieToWatchlist(const uint16_t userId, const uint32_t movieId, const float rating);
 	void RemoveMovieFromWatchlist(const uint16_t userId, const uint32_t movieId);
 	void AddMovieToWishlist(const uint16_t userId, const uint32_t movieId);
 	void RemoveMovieFromWishlist(const uint16_t userId, const uint32_t movieId);
 	void MoveMovieFromWishlistToWatched(const uint16_t userId, const uint32_t movieId, const float rating);
+
 };
