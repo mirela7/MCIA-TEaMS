75586b93bb2edd68a4f31cab370db50517fc856a
SoranaAurelia <sorana.aurelia@gmail.com>
2023-01-14 19:52:00 +0200

    split training and data collection into different functions

--- RecomSystem/RecomSystemRatingBased.py
+++ RecomSystem/RecomSystemRatingBased.py
@@ -2,6 +2,7 @@ import heapq
 import tensorflow
 from NeuralNetwork import retrieve_model
 import numpy as np
+from datetime import datetime
 import csv
 
 
@@ -38,18 +39,37 @@ def test_import_functio():
 
 def train_for_user(userId, movieId, rating):
     userId += 500  # first 500 users are our "base" value
-    with open('../../../../RecomSystem/data/rating_first500.csv', 'a', newline='') as fd:
+    with open('../../../../RecomSystem/data/new_data.csv', 'a', newline='') as fd:
         writer = csv.writer(fd)
-        writer.writerow([userId, movieId, rating])
-    model = tensorflow.keras.models.load_model('../../../../RecomSystem/saved_model/train600')
-    model.compile(optimizer='adam', loss='mean_absolute_error', metrics=['mean_absolute_percentage_error'])
-    model.fit(x=(np.array([userId]), np.array([movieId])), y=np.array([rating]), verbose=0)
-    model.save('../../../../RecomSystem/saved_model/train600')
-    print('updated model!')
+        writer.writerow([userId, movieId, round(rating, 1)])
     return 1
+
+
+def retrain_last_values():
+    train_file = open('data/new_data.csv')
+    csvreader = csv.reader(train_file)
+    userIds = []
+    movieIds = []
+    ratings = []
+    header = next(csvreader)
+    for row in csvreader:
+        userIds.append(int(row[0]))
+        movieIds.append(int(row[1]))
+        ratings.append(float(row[2]))
+    train_file.close()
+    train_started = datetime.now()
+    model = tensorflow.keras.models.load_model('saved_model/train600')
+    model.compile(optimizer='adam', loss='mean_absolute_error', metrics=['mean_absolute_percentage_error'])
+    model.fit(x=(np.array(userIds), np.array(movieIds)), y=np.array(ratings), verbose=1)
+    model.save('saved_model/train600')
+    train_ended = datetime.now()
+
+    with open('logs/train_logs.txt', 'a') as file:
+        file.write('Train started %s and ended %s\n' % (train_started, train_ended))
+    print('trained model!')
 
 
 if __name__ == '__main__':
     # print(recommend_movies(0))
     # print(recommend_movies(0, 100, 10))
-    train_for_user(1, 1, 1)
+    retrain_last_values()
