1d1a0487a17e15ec034d7f6cd1d6e50f501f6b95
SoranaAurelia <sorana.aurelia@gmail.com>
2023-01-15 10:36:54 +0200

    relative path in bat file

--- RecomSystem/retrain.bat
+++ RecomSystem/retrain.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Users\sorana\.conda\envs\recomsys_onlyNN\python.exe" "..\\..\\..\\..\\RecomSystem\\RecomSystemRatingBased_retrain.py"
+"C:\Users\sorana\.conda\envs\recomsys_onlyNN\python.exe" "../../../../RecomSystem/RecomSystemRatingBased_retrain.py"
